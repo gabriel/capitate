0aa6c99787a35fc04ce76d5a08d32aa9029baab1
Gabriel Handford <gabrielh@gmail.com>
2008-01-29 04:00:31 +0000

    fixes
    git-svn-id: http://svn.ducktyper.com/configr/trunk@32 03f83d6c-b416-495f-9003-0924d19d01a8

--- recipes/centos.rb
+++ recipes/centos.rb
@@ -37,31 +37,18 @@ namespace :centos do
     
     with_user(bootstrap_user) do |user_to_add|
     
-      exists = false
-      sudo "id #{user_to_add}" do |channel, stream, data|
-        if data !~ /No such user/
-          exists = true
+      sudo "id sick || /usr/sbin/adduser -d #{deploy_to} -G admin #{user_to_add}"
+      sudo "chmod a+rx #{deploy_to}"
+  
+      new_password = Capistrano::CLI.password_prompt("Password for user (#{user_to_add}): ")
+  
+      sudo "passwd #{user_to_add}" do |channel, stream, data|
+        logger.info data
+    
+        if data =~ /password:/i
+          channel.send_data "#{new_password}\n"
+          channel.send_data "#{new_password}\n"
         end
-      end
-      
-      if !exists
-    
-        sudo "/usr/sbin/adduser -d #{deploy_to} -G admin #{user_to_add}"
-        sudo "chmod a+rx #{deploy_to}"
-    
-        new_password = Capistrano::CLI.password_prompt("Password for user (#{user_to_add}): ")
-    
-        sudo "passwd #{user_to_add}" do |channel, stream, data|
-          logger.info data
-      
-          if data =~ /password:/i
-            channel.send_data "#{new_password}\n"
-            channel.send_data "#{new_password}\n"
-          end
-        end
-        
-      else        
-        logger.important "User #{user_to_add} already exists"
       end
       
     end
--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,16 +1,19 @@
 # Mysql recipes
 namespace :mysql do
   
-  set :mysql_admin_password, Proc.new { Capistrano::CLI.ui.ask('Mysql admin password (to set): ') }
-  
   desc "Install mysql"
   task :install do    
+    
+    set :mysql_admin_password, Proc.new { Capistrano::CLI.ui.ask('Mysql admin password (to set): ') }
+    
     yum_install([ "mysql", "mysql-devel", "mysql-server" ])
     install_script("mysql/install.sh.erb")
   end
   
   desc "Create database user, and database with appropriate permissions"
   task :setup do
+    
+    set :mysql_admin_password, Proc.new { Capistrano::CLI.ui.ask('Mysql admin password: ') }
     
     # Locations can access from
     locations_for_grant = [ "localhost", web_server ]

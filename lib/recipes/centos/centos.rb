cd320a48f90dc96b3203c533390f02814205a40e
Gabriel Handford <gabrielh@gmail.com>
2008-02-22 23:57:17 +0000

    more documentation for recipe

--- lib/recipes/centos/centos.rb
+++ lib/recipes/centos/centos.rb
@@ -1,20 +1,35 @@
 # Custom tasks for centos OS profiles
 namespace :centos do
     
-  desc "Setup centos for web"
+  desc <<-DESC
+  Setup centos for web role.
+  
+  * Create admin group
+  * Install sudoers with access for admin group
+  * Change run level to 3
+  * Create web apps directory at /var/www/apps
+  
+  DESC
   task :setup_for_web do
     put template.load("centos/sudoers"), "/tmp/sudoers"
     script.sh("centos/setup_for_web.sh")
   end  
     
-  desc "Cleanup"
+  desc <<-DESC
+  Cleanup yum.
+  DESC
   task :cleanup do
     yum.clean
-    # TODO: Add cleanup tasks here
+    # TODO: Add more cleanup tasks here
   end
   
   # Add user for an application
-  desc "Add user (adds to admin group)"
+  desc <<-DESC
+  Add user and set user password for application. Adds user to admin group. 
+  
+    set :user, "app_user"
+    
+  DESC
   task :add_user_for_app do
     
     # Settings

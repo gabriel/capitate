5493a2eeacdb9361b37bb3c702d3bdc1a93222e6
Gabe <gabrielh@gmail.com>
2008-03-18 18:46:04 -0400

    adding database_yml_template param and moving log recipes into namespace

--- lib/recipes/rails.rb
+++ lib/recipes/rails.rb
@@ -2,7 +2,8 @@
 namespace :rails do
   
   desc <<-DESC
-  Create database yaml in shared path.
+  Create database yaml in shared path. Note: If both <tt>:db_host</tt> and <tt>:db_socket</tt> are used,
+  db_socket wins.
   
   *db_name*: Database name (rails).\n    
   @set :db_name, "app_db_name"@\n  
@@ -11,8 +12,9 @@ namespace :rails do
   *db_pass*: Database password (rails).\n  
   @set :db_pass, "the_password"@\n    
   *db_host*: Database host (can be nil, if you are using socket). _Defaults to nil_\n
-  *db_socket*: Database socket (can be nil, if you are using host). _Defaults to nil_\n
+  *db_socket*: Database socket (can be nil, if you are using host). _Defaults to nil_\n    
   @set :db_socket, "/var/lib/mysql/mysql.sock"@\n
+  *database_yml_template*: Path to database yml erb template. _Defaults to <tt>rails/database.yml.erb</tt>_ (in this GEM)\n
   DESC
   task :setup do    
     
@@ -22,6 +24,7 @@ namespace :rails do
     fetch(:db_pass)
     fetch_or_default(:db_host, nil)
     fetch_or_default(:db_socket, nil)
+    fetch_or_default(:database_yml_template, "rails/database.yml.erb")
     
     unless db_host.blank?
       set :db_connect_type, "host"
@@ -34,7 +37,7 @@ namespace :rails do
     end
     
     run "mkdir -p #{shared_path}/config"
-    put template.load("rails/database.yml.erb"), "#{shared_path}/config/database.yml"
+    put template.load(database_yml_template), "#{shared_path}/config/database.yml"
   end
 
   desc "Make symlink for database yaml" 
@@ -42,43 +45,50 @@ namespace :rails do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
   end
   
-  desc <<-DESC
-  Tail production log files. (http://errtheblog.com/posts/19-streaming-capistrano)
-  DESC
-  task :tail_logs, :roles => :web do
-    run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
-      puts  # for an extra line break before the host name
-      puts "#{channel[:host]}: #{data}" 
-      break if stream == :err    
+  # Log tasks
+  namespace :logs do 
+    
+    desc <<-DESC
+    Tail production log files.\n
+    http://errtheblog.com/posts/19-streaming-capistrano
+    DESC
+    task :tail, :roles => :web do
+      run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+        puts  # for an extra line break before the host name
+        puts "#{channel[:host]}: #{data}" 
+        break if stream == :err    
+      end
+    end
+
+
+    desc <<-DESC
+    Check production log files in TextMate.\n
+    See http://errtheblog.com/posts/19-streaming-capistrano
+    DESC
+    task :mate, :roles => :app do
+
+      require 'tempfile'
+      tmp = Tempfile.open('w')
+      logs = Hash.new { |h,k| h[k] = '' }
+
+      run "tail -n500 #{shared_path}/log/production.log" do |channel, stream, data|
+        logs[channel[:host]] << data
+        break if stream == :err
+      end
+
+      logs.each do |host, log|
+        tmp.write("--- #{host} ---\n\n")
+        tmp.write(log + "\n")
+      end
+
+      exec "mate -w #{tmp.path}" 
+      tmp.close
     end
   end
 
-
   desc <<-DESC
-  Check production log files in TextMate.  (http://errtheblog.com/posts/19-streaming-capistrano)
-  DESC
-  task :mate_logs, :roles => :app do
-
-    require 'tempfile'
-    tmp = Tempfile.open('w')
-    logs = Hash.new { |h,k| h[k] = '' }
-
-    run "tail -n500 #{shared_path}/log/production.log" do |channel, stream, data|
-      logs[channel[:host]] << data
-      break if stream == :err
-    end
-
-    logs.each do |host, log|
-      tmp.write("--- #{host} ---\n\n")
-      tmp.write(log + "\n")
-    end
-
-    exec "mate -w #{tmp.path}" 
-    tmp.close
-  end
-
-  desc <<-DESC
-  Remotely console. (http://errtheblog.com/posts/19-streaming-capistrano)
+  Remotely console.\n
+  See http://errtheblog.com/posts/19-streaming-capistrano
   DESC
   task :console, :roles => :app do
     input = ''

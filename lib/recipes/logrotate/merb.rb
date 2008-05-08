4b905ab54f15ceb58b07c36601aa0d2e3be094bc
Gabe <gabrielh@gmail.com>
2008-05-08 15:40:35 -0400

    minor fixes

--- lib/recipes/logrotate/merb.rb
+++ lib/recipes/logrotate/merb.rb
@@ -7,12 +7,12 @@ namespace :merb do
     <dl>
     <dt>merb_logrotate_path</dt>
     <dd>Merb logrotate path</dd>
-    <dd class="default">Defaults to @\#{shared_path}/log/merb_*.log@</dd>
+    <dd class="default">Defaults to @\#{shared_path}/log/merb.*.log@</dd>
     </dl>
     "Source":#{link_to_source(__FILE__)}    
     DESC
     task :install do
-      fetch_or_default(:merb_logrotate_path, "#{shared_path}/log/merb_*.log")
+      fetch_or_default(:merb_logrotate_path, "#{shared_path}/log/merb.*.log")
     
       set :logrotate_name, "merb_#{application}"
       set :logrotate_log_path, merb_logrotate_path
